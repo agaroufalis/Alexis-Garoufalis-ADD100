498151f55ea4bb89a0f840fc4a3a1b95c708f075
agaroufalis <agaroufalis@students.mchenry.edu>
2026-01-26 23:23:54 -0600

    Uploaded RPG Character Profile

---
+++ RPG_Character_Profile.py
@@ -0,0 +1,25 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable.
+[ ] 3. Define an Integer variable.
+[ ] 4. Define a Float variable.
+[ ] 5. Define a Boolean variable.
+[ ] 6. Print all variables using F-Strings.
+[ ] 7. Upload to GitHub.
+-----------------------------------------------------------------------
+"""
+
+#Define Variables
+name = "Vireen"  #String
+level = 6        #Imteger
+speed = 46.5     #float
+alive = True     #bool
+
+#print f-strings
+print(f"Character Name: {name}")
+print(f"Character level: level {level} ")
+print(f"Character movement speed: {speed} feet per turn")
+print(f"Character life status, are they alive? {alive}")
